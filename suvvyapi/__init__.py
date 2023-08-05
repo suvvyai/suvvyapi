3eb471bf7654450ded761115e72022f7b1422c94
sushka <barabum@duck.com>
2023-08-05 23:53:41 +0300

    :sparkles: AsyncSuvvyAPIWrapper

--- suvvyapi/__init__.py
+++ suvvyapi/__init__.py
@@ -1,3 +1,4 @@
 from suvvyapi.sync.wrapper import SuvvyAPIWrapper
+from suvvyapi.asynchronous.wrapper import AsyncSuvvyAPIWrapper
 from suvvyapi.models.history import *
 from suvvyapi.models.responses import *
