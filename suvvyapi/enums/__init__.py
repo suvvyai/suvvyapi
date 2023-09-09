f0a68817c205f13a6b37a5a707bffdefb6cfc526
sushka <barabum@duck.com>
2023-09-09 18:53:32 +0300

    :sparkles: add roles

--- suvvyapi/enums/__init__.py
+++ suvvyapi/enums/__init__.py
@@ -1,8 +1,10 @@
 from enum import Enum
 
 
-class Role(Enum):
+class Role(str, Enum):
     HUMAN = "human"
     AI = "ai"
     FUNCTION_CALL = "function_call"
     FUNCTION_RESULT = "function_result"
+    IMAGE_MESSAGE = "sent_image"
+    AUDIO_MESSAGE = "audio_message"
