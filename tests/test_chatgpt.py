a54c77c16462295e407bce4a7cd9c097caa28590
roman mac <barabum@duck.com>
2023-12-22 18:53:05 +0300

    ✨ feat(tests): add async tests and refactor sync tests

--- tests/test_chatgpt.py
+++ tests/test_chatgpt.py
@@ -5,21 +5,42 @@ import string
 
 from suvvyapi import SuvvyAPIWrapper, Message, AsyncSuvvyAPIWrapper
 
-suvvy_1 = SuvvyAPIWrapper(token=os.getenv("TEST1_SUVVY_TOKEN"))
-async_suvvy_1 = AsyncSuvvyAPIWrapper(token=os.getenv("TEST1_SUVVY_TOKEN"))
+suvvy = SuvvyAPIWrapper(token=os.getenv("TEST1_SUVVY_TOKEN"))
+async_suvvy = AsyncSuvvyAPIWrapper(token=os.getenv("TEST1_SUVVY_TOKEN"))
+
+
+def generate_unique_id() -> str:
+    return f"pytest-{datetime.datetime.utcnow().isoformat()}-{''.join(random.choices(string.digits + string.ascii_letters, k=50))}"
 
 
 def test_sync_message_responded():
-    unique_id = f"pytest-{datetime.datetime.utcnow().isoformat()}-{random.choices(string.digits + string.ascii_letters, k=50)}"
-    response = suvvy_1.predict(Message(text="Hello"), unique_id=unique_id)
+    unique_id = generate_unique_id()
+    response = suvvy.predict(Message(text="Hello"), unique_id=unique_id)
 
     assert response.actual_response is not None
 
 
 def test_sync_message_interrupted():
-    unique_id = f"pytest-{datetime.datetime.utcnow().isoformat()}-{random.choices(string.digits + string.ascii_letters, k=50)}"
+    unique_id = generate_unique_id()
 
     # Interrupt reason - control phrase
-    response = suvvy_1.predict(Message(text="Хорошо, подождите минутку"), unique_id=unique_id)
+    response = suvvy.predict(Message(text="Хорошо, подождите минутку"), unique_id=unique_id)
 
     assert response.actual_response is None
+
+
+async def test_async_message_responded():
+    unique_id = generate_unique_id()
+    response = await async_suvvy.predict(Message(text="Hello"), unique_id=unique_id)
+
+    assert response.actual_response is not None
+
+
+async def test_async_message_interrupted():
+    unique_id = generate_unique_id()
+
+    # Interrupt reason - control phrase
+    response = await async_suvvy.predict(Message(text="Хорошо, подождите минутку"), unique_id=unique_id)
+
+    assert response.actual_response is None
+
