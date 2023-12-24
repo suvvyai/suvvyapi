e8264b022dcc084f4e7cae5d2e1a3ae99861c448
roman mac <barabum@duck.com>
2023-12-24 19:40:01 +0300

    🛠️ fix(init): add missing Suvvy import to __init__.py

--- suvvyapi/__init__.py
+++ suvvyapi/__init__.py
@@ -2,3 +2,4 @@ from suvvyapi.asynchronous.wrapper import AsyncSuvvyAPIWrapper
 from suvvyapi.models.history import *
 from suvvyapi.models.responses import *
 from suvvyapi.sync.wrapper import SuvvyAPIWrapper
+from suvvyapi.wrapper import Suvvy
