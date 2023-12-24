17f2b6fa82acf2af651f5b81fd418145fad23de7
roman mac <barabum@duck.com>
2023-12-24 19:00:32 +0300

    Revert "🔄 refactor: rename suvvyapi package to suvvyapi-old and add Suvvy class"
    This reverts commit 0c0a014708c711ff89c7291335e5cf6499168a24.

--- suvvyapi-old/wrapper.py
+++
@@ -1,6 +0,0 @@
-
-
-
-class Suvvy(object):
-    def __init__(self):
-        pass
--- suvvyapi/__init__.py
+++ suvvyapi/__init__.py
@@ -2,4 +2,3 @@ from suvvyapi.asynchronous.wrapper import AsyncSuvvyAPIWrapper
 from suvvyapi.models.history import *
 from suvvyapi.models.responses import *
 from suvvyapi.sync.wrapper import SuvvyAPIWrapper
-from suvvyapi.wrapper import Suvvy
