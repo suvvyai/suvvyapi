47a6997e0a016e795d70dfc8af70d74f505ec46a
roman mac <barabum@duck.com>
2023-12-26 19:44:58 +0300

    🚀 fix(imports): add noqa comments to suppress linter warnings

--- suvvyapi/__init__.py
+++ suvvyapi/__init__.py
@@ -1,5 +1,5 @@
-from suvvyapi.asynchronous.wrapper import AsyncSuvvyAPIWrapper
-from suvvyapi.models.history import *
-from suvvyapi.models.responses import *
-from suvvyapi.sync.wrapper import SuvvyAPIWrapper
-from suvvyapi.wrapper import Suvvy
+from suvvyapi.asynchronous.wrapper import AsyncSuvvyAPIWrapperv  # noqa: F401
+from suvvyapi.models.history import *  # noqa: F403
+from suvvyapi.models.responses import *  # noqa: F403
+from suvvyapi.sync.wrapper import SuvvyAPIWrapper  # noqa: F401
+from suvvyapi.wrapper import Suvvy  # noqa: F401
