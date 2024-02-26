b8239ea8d6b36112035f438aa95a3439ef682d13
roman mac <barabum@duck.com>
2024-02-26 17:37:10 +0300

    ✨ feat: добавил модели сообщений

---
+++ suvvyapi/models/message.py
@@ -0,0 +1,16 @@
+from typing import Literal
+
+from pydantic import BaseModel
+
+from suvvyapi.models.enums import SenderRole
+from suvvyapi.models.message_data.text import TextMessageData
+
+
+class Message(BaseModel):
+    message_sender: SenderRole
+    message_data: TextMessageData
+
+
+class RequestMessage(Message):
+    message_sender: Literal[SenderRole.CUSTOMER, SenderRole.EMPLOYEE]
+    
