11de45034d0f4cd8b78e506c2a776470b7a6f3ff
roman mac <barabum@duck.com>
2024-02-26 18:16:13 +0300

    🛠 refactor(imports): упростил импорты в `__init__.py`

--- suvvyapi/__init__.py
+++ suvvyapi/__init__.py
@@ -1,29 +1,15 @@
-from suvvyapi.asynchronous.wrapper import AsyncSuvvyAPIWrapper
-from suvvyapi.models.history import (
-    ChatHistory,
-    Message,
-    HistoryMessage,
-    FunctionDetails,
-)
-from suvvyapi.models.responses import (
-    Prediction,
+from suvvyapi.models.message import Message
+from suvvyapi.models.token_usage import (
     LLMResult,
     TokenUsage,
     BalanceUsage,
 )
-from suvvyapi.sync.wrapper import SuvvyAPIWrapper
 from suvvyapi.wrapper import Suvvy
 
 __all__ = [
-    "AsyncSuvvyAPIWrapper",
-    "SuvvyAPIWrapper",
     "Suvvy",
-    "Prediction",
     "LLMResult",
     "TokenUsage",
     "BalanceUsage",
-    "ChatHistory",
     "Message",
-    "HistoryMessage",
-    "FunctionDetails",
 ]
