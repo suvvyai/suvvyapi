081563a99bcac1b85a8e6d9043cf8a1915f7d1c3
roman mac <barabum@duck.com>
2024-02-26 18:04:56 +0300

    ✨ feat(models): добавил класс `DialogueMessage` и импорты

--- suvvyapi/models/message.py
+++ suvvyapi/models/message.py
@@ -1,6 +1,7 @@
+from datetime import datetime
 from typing import Literal
 
-from pydantic import BaseModel
+from pydantic import BaseModel, UUID4, Field
 
 from suvvyapi.models.enums import SenderRole
 from suvvyapi.models.message_data.text import TextMessageData
@@ -13,3 +14,12 @@ class Message(BaseModel):
 
 class RequestMessage(Message):
     message_sender: Literal[SenderRole.CUSTOMER, SenderRole.EMPLOYEE]
+
+
+class DialogueMessage(Message):
+    message_id: UUID4 = Field(frozen=True)
+    tokens: int = Field(frozen=True)
+    created_at: datetime = Field(frozen=True)
+
+    message_sender: SenderRole = Field(frozen=True)
+    message_data: TextMessageData = Field(frozen=True)
