b1b6f29ed966e48f4158697ffc674de29b027368
roman mac <barabum@duck.com>
2024-02-26 17:32:24 +0300

    ✨ feat: добавил новый enum `ContentType` и обновил `__init__.py`

--- suvvyapi/models/enums/__init__.py
+++ suvvyapi/models/enums/__init__.py
@@ -1,3 +1,4 @@
 from suvvyapi.models.enums.role import SenderRole
+from suvvyapi.models.enums.content_type import ContentType
 
-__all__ = ["SenderRole"]
+__all__ = ["SenderRole", "ContentType"]
---
+++ suvvyapi/models/enums/content_type.py
@@ -0,0 +1,18 @@
+from enum import StrEnum
+
+
+class ContentType(StrEnum):
+    TEXT = "text"
+    """Текстовое сообщение. Соответствует `TextMessageData`"""
+
+    IMAGE = "image"
+    """Изображение. Соответствует `ImageMessageData`"""
+
+    AUDIO = "audio"
+    """Аудиосообщение. Соответствует `AudioMessageData`"""
+
+    TOOL_CALLS = "tool_calls"
+    """Вызов одной/нескольких функций. Соответствует `ToolCallsMessageData`"""
+
+    TOOL_RESPONSE = "tool_response"
+    """Результат выполнения функции. Соответствует `ToolResponseMessageData`"""
